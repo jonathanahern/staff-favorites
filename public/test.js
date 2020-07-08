b4a7df3195343b76f36dd5db39df54e84d79c5c7
jonathanahern <modestfool@gmail.com>
2020-07-07 23:29:20 -0400

    Add media phone style

--- public/test.js
+++ public/test.js
@@ -55,14 +55,39 @@ function setupPageForPick(){
             'width: 80%;' +
         '}' +
         '#full_container {' +
-            'display: block;' +
+            'display: flex;' +
         '}' +
         '#staff_pick_ele {' +
             'width: 20%;' +
+            'margin-left: 20px;' +
         '}' +
-        '@media screen and(max - width: 850px) {'+
-            'body{' +
-            'background-color: blue;' +
+        '#staff_pick_ele h1{' +
+            'text-align: center;' +
+        '}' +
+        '@media screen and (max-width: 750px) {'+
+            '#full_container {' +
+                'display: block;' +
+            '}' +
+            '#staff_pick_ele {' +
+                'display: flex;' +
+                'justify-content: center;' +
+                'flex-direction: column;' +
+                'align-items: center;' +
+                'width: 75%;' +
+                'margin: 0 auto;' +          
+            '}' +
+            '#staff_pick_ele img {' +
+                'max-width: 150px;' +
+                'max-height: 260px;' +
+            '}' +
+            '#staff_pick_ele h1 {' +
+                'text-align: center;' +
+            '}' +
+            '#staff_pick_ele > div {' +
+            'display:flex;' +
+            '}' +
+            '#staff_pick_ele > div > div{' +
+            'padding: 0 20px;' +
             '}' +
         '}';
 
@@ -71,7 +96,7 @@ function setupPageForPick(){
 
     // Insert our new styles before the first script tag
     ref.parentNode.insertBefore(style, ref);
-    $('head').append('<style type="text/css">@media screen and (max-width: 1012px){ background-color: blue;}</style>');
+    // $('head').append('<style type="text/css">@media screen and (max-width: 600px) {#staff_pick_ele{ background-color: blue;}}</style>');
 
     // fullContainer.style.display = "flex";
     // mainContent.style.width = "80%";
@@ -83,23 +108,29 @@ function setupPageForPick(){
 function insertData(data){
     const staffPick = document.getElementById(`staff_pick_ele`);
 
-    let h3 = document.createElement("h1");
-    h3.innerHTML = "Staff Pick";
-    staffPick.appendChild(h3);
+    let h1 = document.createElement("h1");
+    h1.innerHTML = "Staff Pick!";
+    staffPick.appendChild(h1);
+
+    const staffDiv = document.createElement("div");
+    staffPick.appendChild(staffDiv);
 
     let img = document.createElement("img");
     img.src = data["profile_url"];
-    staffPick.appendChild(img);
+    staffDiv.appendChild(img);
+
+    const wordsDiv = document.createElement("div");
+    staffDiv.appendChild(wordsDiv);
 
     let pDescription = document.createElement("p");
     pDescription.innerHTML = data["review"];
     pDescription.style.fontStyle = "italic";
     pDescription.style.margin = "0px";
-    staffPick.appendChild(pDescription);
+    wordsDiv.appendChild(pDescription);
 
     let pName = document.createElement("p");
     let nameString = `-${data["name"]}`
     pName.innerHTML = nameString;
-    staffPick.appendChild(pName);
+    wordsDiv.appendChild(pName);
 
 }
