f5dce1561728ece19a8a862460e41e0e085d7731
jonathanahern <modestfool@gmail.com>
2020-10-15 15:49:16 -0400

    Back to basic test

--- app/controllers/api/front_end_controller.rb
+++ app/controllers/api/front_end_controller.rb
@@ -1,13 +1,12 @@
 class Api::FrontEndController < ApplicationController
 
     def index
-        # render json: {"try again index": "works"}
-        @shopsProducts = Product.getProductIDsSettings(params[:shop])
-        render json: @shopsProducts
+        render json: {"try again index": params[:shop]}
+        # @shopsProducts = Product.getProductIDsSettings(params[:shop])
+        # render json: @shopsProducts
     end
 
     def show
-        render json: {"try again show": "works"}
         @picks = Product.pickInfo(params[:prodID]).first
         render json: @picks
     end
