50f9adda30db0b5c8b7115d2729e02b7ea65ff20
jonathanahern <modestfool@gmail.com>
2020-10-15 18:08:35 -0400

    Try new shop name

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: :any
-  end
+#   allow do
+#     origins 'shop'
+#     resource '*', headers: :any, methods: :any
+#   end
 end
