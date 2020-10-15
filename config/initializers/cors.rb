b68d992bae02cbae6b968ce97001553589a3e052
jonathanahern <modestfool@gmail.com>
2020-10-15 17:41:10 -0400

    Try colon

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
-    credentials true
+    credentials: true
   end
 end
